7b48e3bb7aaca9876cdf6e6c0553695cae0943ff
Liminghing <im349412@gmail.com>
2024-11-15 18:06:32 +0900

    add UnitTest

---
+++ app/src/main/java/com/jkweyu/kotlin_basic/UnitTest.kt
@@ -0,0 +1,26 @@
+package com.jkweyu.kotlin_basic
+import junit.framework.Assert.assertEquals
+import org.junit.Test
+
+class UnitTest {
+}
+class CalculatorTest {
+
+    // 테스트하려는 메서드
+    fun add(a: Int, b: Int): Int {
+        return a + b
+    }
+
+    @Test
+    fun testAdd() {
+        // add 함수가 의도한 대로 동작하는지 테스트
+        val result = add(2, 3)
+        assertEquals(5, result) // 예상값 5와 비교
+    }
+
+    @Test
+    fun testAddNegativeNumbers() {
+        val result = add(-2, -3)
+        assertEquals(-5, result) // 예상값 -5와 비교
+    }
+}
