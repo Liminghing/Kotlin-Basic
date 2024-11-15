f4f9452a638fa1b8abea46fa0ba9560bd72c996b
Liminghing <im349412@gmail.com>
2024-11-15 17:03:38 +0900

    수

--- app/src/main/java/com/jkweyu/kotlin_basic/HigherOrderFunction.kt
+++ app/src/main/java/com/jkweyu/kotlin_basic/HigherOrderFunction.kt
@@ -7,6 +7,10 @@ fun main() {
     println(sum) // 출력: 7
     val result = pow2(::calculate, 3)  //::를 사용해 calculate를 호출하지 않고 calculate를 참조만 해줌
     println(result)
+
+    //val multiplyBy2: (Int) -> Int 인 함수
+    val multiplyBy2 = multiplyBy(2) // multiplyBy 함수가 (Int) -> Int 형식의 함수를 반환
+    println(multiplyBy2(5)) // 출력: 10
 }
 fun calculate(operation: (Int, Int) -> Int, a: Int, b: Int): Int {
     return operation(a, b)
@@ -15,3 +19,6 @@ fun calculate(operation: (Int, Int) -> Int, a: Int, b: Int): Int {
 fun pow2(calculate: (operation: (Int, Int) -> Int, a: Int, b: Int) -> Int, x: Int): Int {
     return calculate({ num1, num2 -> num1 * num2 }, x, x)
 }
+fun multiplyBy(factor: Int): (Int) -> Int {
+    return { number -> number * factor }
+} //함수반환 : (Int) -> Int
